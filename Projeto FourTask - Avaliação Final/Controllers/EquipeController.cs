ab5060033d9280cc7db68bcc197f3b4370a4b417
Pâmela Gomes Mojolla <pmojolla@server01.foursys.local>
2022-06-21 10:52:25 -0300

    Projeto_Final_Foursys

---
+++ Projeto FourTask - Avaliação Final/Controllers/EquipeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto_FourTask___Avaliação_Final.Controllers
+{
+    public class EquipeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
